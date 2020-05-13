1f4898faf70c9e1775585ea54b7ee1ec5d57f026
Tien0028 <tien0028@easv365.dk>
2020-05-13 16:37:43 +0200

    ImageFinder in EditProfile

--- src/tempus/gui/controller/SettingsWindowController.java
+++ src/tempus/gui/controller/SettingsWindowController.java
@@ -7,12 +7,20 @@ package tempus.gui.controller;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
+import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import tempus.be.User;
+import tempus.gui.model.UserModel;
 
 /**
  * FXML Controller class
@@ -20,6 +28,7 @@ import javafx.scene.image.ImageView;
  * @author dpank
  */
 public class SettingsWindowController implements Initializable {
+    private final List<Image> images = new ArrayList<>();
 
     @FXML
     private JFXButton btnClose;
@@ -33,25 +42,48 @@ public class SettingsWindowController implements Initializable {
     private ImageView imgPhoto;
     @FXML
     private JFXButton btnEditPic;
+    
+    private User user;
+    private UserModel userModel;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        userModel = UserModel.getInstance();
+        showImg();
     }    
 
     @FXML
     private void onClickClose(ActionEvent event) {
+        goBack();
     }
 
     @FXML
     private void onClickConfirm(ActionEvent event) {
+        
     }
 
     @FXML
-    private void onClickOpenPicSearcher(ActionEvent event) {
+    private void onClickOpenPicSearcher(ActionEvent event) 
+    {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select image files");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Images", 
+            "*.png", "*.jpg", "*.gif", "*.tif", "*.bmp"));        
+        List<File> files = fileChooser.showOpenMultipleDialog(new Stage());
+        showImg();
+    }
+
+    private void showImg() {
+        User us = userModel.getloggedInUser();
+        imgPhoto.setImage(us.getPhotoURL());
+    }
+
+    private void goBack() {
+        Stage stage = (Stage) btnClose.getScene().getWindow();
+        stage.close();
     }
     
 }
