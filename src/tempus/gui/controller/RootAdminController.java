410ce0fd925509d3c66775a519582cd3bc9e86bc
dpankov91 <dpankov91@gmail.com>
2020-05-13 11:23:22 +0300

    help window

--- src/tempus/gui/controller/RootAdminController.java
+++ src/tempus/gui/controller/RootAdminController.java
@@ -104,11 +104,15 @@ public class RootAdminController implements Initializable {
     }
 
     @FXML
-    private void handle_Help(ActionEvent event) {
-//        System.out.println("Clicked");
-//        FxmlLoader object = new FxmlLoader();
-//        Pane view = object.getPage("");
-//        mainPane.setCenter(view); 
+    private void handle_Help(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/tempus/gui/view/HelpWindow.fxml"));
+        Parent z = loader.load();
+        Scene scene = new Scene(z);
+        Stage s = new Stage();
+        s.setScene(scene);
+        s.show();
+        Stage stage = (Stage) btnLogOut.getScene().getWindow();
+        stage.close();
 
     }
 
