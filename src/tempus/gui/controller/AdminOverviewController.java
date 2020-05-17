eca1cf633163cf524378f143966baadbac523694
dpankov91 <dpankov91@gmail.com>
2020-05-18 01:15:37 +0300

    overview done

--- src/tempus/gui/controller/AdminOverviewController.java
+++ src/tempus/gui/controller/AdminOverviewController.java
@@ -9,12 +9,19 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXDatePicker;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -43,9 +50,7 @@ import tempus.gui.model.UserModel;
  */
 public class AdminOverviewController implements Initializable {
 
-    @FXML
     private JFXComboBox<String> cmbDateRange;
-    @FXML
     private JFXComboBox<String> cmbUserOrProject;
     private StackedBarChart<String, Integer> chartBar;
     @FXML
@@ -73,9 +78,8 @@ public class AdminOverviewController implements Initializable {
     @FXML
     private JFXComboBox<User> cmbUsers;
 
-    
-    ObservableList<Project>  allProjects = FXCollections.observableArrayList();
-    ObservableList<User>  allUsers = FXCollections.observableArrayList();
+    ObservableList<Project> allProjects = FXCollections.observableArrayList();
+    ObservableList<User> allUsers = FXCollections.observableArrayList();
     private ProjectModel projModel;
     private UserModel usModel;
     private TaskModel taskModel;
@@ -83,86 +87,70 @@ public class AdminOverviewController implements Initializable {
     private LocalDate toDate;
     @FXML
     private JFXButton btnShow;
-    
- 
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        fromDate = dateFrom.getValue();
-        toDate = dateTo.getValue();
         projModel = ProjectModel.getInstance();
         usModel = UserModel.getInstance();
         taskModel = TaskModel.getInstance();
         loadProjectsToCombobox();
         loadUsersToCombobox();
-        cmbDateRange.setItems(FXCollections.observableArrayList(
-                "This Week", "This Month"));
-        cmbUserOrProject.setItems(FXCollections.observableArrayList(
-                "Project", "User"));
+//        cmbDateRange.setItems(FXCollections.observableArrayList(
+//                "This Week", "This Month"));
+//        cmbUserOrProject.setItems(FXCollections.observableArrayList(
+//                "Project", "User"));
         setUpTaskTableView();
-        loadDataForAllProjectsInChart();
-    }    
 
-
-    @FXML
-    private void onSelectAppearUserOrProjectTable(ActionEvent event) 
-    {
-        
     }
 
+
     @FXML
-    private void onSelectLoadSelectedProjectTable(ActionEvent event) 
-    {
+    private void onSelectLoadSelectedProjectTable(ActionEvent event) {
         //cmbUsers.getSelectionModel().clearSelection();
         Project pro = cmbProjects.getSelectionModel().getSelectedItem();
         loadSelectedProjectTableView(pro);
     }
-    
-     @FXML
+
+    @FXML
     private void onSelectLoadSelectedUserTable(ActionEvent event) {
         //cmbProjects.getSelectionModel().clearSelection();
         User us = cmbUsers.getSelectionModel().getSelectedItem();
         loadSelectedUsersTableView(us);
     }
-    
+
     @FXML
     private void onClickLoadAllProjectsTable(ActionEvent event) {
         setUpTaskTableView();
     }
 
-    @FXML
-    private void onSelectSetDateRangeChartBar(ActionEvent event) {
-    }
-    
-    private void loadProjectsToCombobox() 
-    { 
+
+    private void loadProjectsToCombobox() {
         allProjects = projModel.getObsProjects();
-        
+
         for (Project proj : allProjects) {
             cmbProjects.setItems(allProjects);
-        }    
+        }
     }
-    
-    private void loadUsersToCombobox() 
-    { 
+
+    private void loadUsersToCombobox() {
         allUsers = usModel.getObsUsers();
-        
+
         for (User us : allUsers) {
             cmbUsers.setItems(allUsers);
-        }    
+        }
     }
-    
-    private void loadSelectedUsersTableView(User us){
+
+    private void loadSelectedUsersTableView(User us) {
         tableProject.getItems().clear();
         List<Task> allTasksOfSelectedUser = taskModel.getTasksOfSelectedUser(us);
         ObservableList<Task> tasks = FXCollections.observableArrayList();
         tasks.addAll(allTasksOfSelectedUser);
         tableProject.setItems(tasks);
     }
-    
+
     private void loadAllTaskTableView() {
         tableProject.getItems().clear();
         List<Task> allTasksOverview = taskModel.getAllTasksOverview();
@@ -170,15 +158,15 @@ public class AdminOverviewController implements Initializable {
         tasks.addAll(allTasksOverview);
         tableProject.setItems(tasks);
     }
-    
-    private void loadSelectedProjectTableView(Project selectedProject){
+
+    private void loadSelectedProjectTableView(Project selectedProject) {
         tableProject.getItems().clear();
         List<Task> allTasksOfSelectedProject = taskModel.getTasksOfSelectedProject(selectedProject);
         ObservableList<Task> tasks = FXCollections.observableArrayList();
         tasks.addAll(allTasksOfSelectedProject);
         tableProject.setItems(tasks);
     }
-    
+
     private void setUpTaskTableView() {
         colName.setCellValueFactory(new PropertyValueFactory<>("projName"));
         colTask.setCellValueFactory(new PropertyValueFactory<>("task"));
@@ -188,34 +176,73 @@ public class AdminOverviewController implements Initializable {
         loadAllTaskTableView();
     }
 
-    
-    
-    private void loadDataForAllProjectsInChart(){
+    private void loadDataForAllProjectsInChart() {
         paneBarChart.getChildren().clear();
+
         CategoryAxis xAxis = new CategoryAxis();
         xAxis.setLabel("Week Days");
+
         NumberAxis yAxis = new NumberAxis();
-        yAxis.setLabel("Hours");
+        yAxis.setLabel("Minutes");
+
         BarChart weekProject = new BarChart(xAxis, yAxis);
         weekProject.setTitle("This week projects stats");
+
         XYChart.Series series = new XYChart.Series();
-        series.setName("Hours Amount");
+        series.setName("Time Spent Each Day");
         
-        series.getData().add(new XYChart.Data<>("Monday", 54));
-     
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MMM-dd");
+
+
+        List<LocalDate> datesToIterate = getDifferenceDays(dateFrom.getValue(), dateTo.getValue());
+        List<Task> taskList = taskModel.getTasksBetween(dateFrom.getValue(), dateTo.getValue());
+        // get tasks
+        for (LocalDate localDate : datesToIterate) {
+            for (Task task : taskList) {
+                LocalDate createDate = new java.sql.Date( task.getCreatedDate().getTime() ).toLocalDate();
+                if (formatter.format(localDate).equals(formatter.format(createDate))) {
+                    series.getData().add(new XYChart.Data<>(localDate.toString(), task.getSpentTime()));
+                  //  break;
+                }
+
+            }
+
+        }
         weekProject.getData().add(series);
         paneBarChart.getChildren().add(weekProject);
     }
 
-    
+//    private final DateTimeFormatter dateTimeFormatter=DateTimeFormatter.ofPattern("dd/MM/yyyy");
+//
+//    public String toString(LocalDate localDate)
+//    {
+//        if(localDate==null)
+//            return "";
+//        return dateTimeFormatter.format(localDate);
+//    }
+//
+//    public LocalDate fromString(String dateString)
+//    {
+//        if(dateString==null || dateString.trim().isEmpty())
+//        {
+//            return null;
+//        }
+//        return LocalDate.parse(dateString,dateTimeFormatter);
+//    }
+    private List<LocalDate> getDifferenceDays(LocalDate fromDate, LocalDate toDate) {
+
+        return Stream.iterate(fromDate, date -> date.plusDays(1))
+                .limit(ChronoUnit.DAYS.between(fromDate, toDate))
+                .collect(Collectors.toList());
+    }
+
     @FXML
     private void onClickShowBarChart(ActionEvent event) {
+        loadDataForAllProjectsInChart();
     }
-    
-    private long daysBeetwinDates(){
-        long daysBetween = Duration.between(fromDate, toDate).toDays();
-        return daysBetween;
+
+    @FXML
+    private void formateDate(ActionEvent event) {
+
     }
-    
-    
 }
--- src/tempus/gui/model/TaskModel.java
+++ src/tempus/gui/model/TaskModel.java
@@ -7,9 +7,12 @@ package tempus.gui.model;
 
 import java.sql.Date;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import tempus.be.Project;
 import tempus.be.Task;
 import tempus.be.User;
@@ -21,7 +24,7 @@ import tempus.bll.IBllFacade;
  * @author dpank
  */
 public class TaskModel {
-    
+
     static TaskModel model = new TaskModel();
     private final IBllFacade bllManager;
     List<Task> alltasks = new ArrayList();
@@ -35,60 +38,78 @@ public class TaskModel {
     }
 
     public List<Task> getAllTasksOverview() {
-        alltasks=bllManager.getAllTasksOverview();
+        alltasks = bllManager.getAllTasksOverview();
         return alltasks;
     }
 
-
-    public List<Task> getTasksOfSelectedProject(Project selectedProject) 
-    {
+    public List<Task> getTasksOfSelectedProject(Project selectedProject) {
         List<Task> allspecTasks = new ArrayList();
         for (Task alltas : alltasks) {
-            if(alltas.getProjName().equals(selectedProject.getName())){
+            if (alltas.getProjName().equals(selectedProject.getName())) {
                 allspecTasks.add(alltas);
             }
         }
         return allspecTasks;
     }
-    
+
     public List<Task> getTasksOfSelectedUser(User us) {
-        
+
         List<Task> allspecUsTasks = new ArrayList();
         for (Task alltas : alltasks) {
-            if(alltas.getUserLastName().equals(us.getLName())){
+            if (alltas.getUserLastName().equals(us.getLName())) {
                 allspecUsTasks.add(alltas);
             }
         }
         return allspecUsTasks;
     }
 
-    
-    public List<Task> getTasksBetween(LocalDate fromDate, LocalDate toDate) 
-    {
+    public List<Task> getTasksBetween(LocalDate fromDate, LocalDate toDate) {
         List<Task> TasksBetween = new ArrayList();
         Date dateFrom = java.sql.Date.valueOf(fromDate);
         Date dateTo = java.sql.Date.valueOf(toDate);
-        
+
         for (Task alltas : alltasks) {
-            if(alltas.getCreatedDate().after(dateFrom) && alltas.getCreatedDate().before(dateFrom)){
+            if ((alltas.getCreatedDate().after(dateFrom) || alltas.getCreatedDate().equals(dateFrom)) && (alltas.getCreatedDate().before(dateTo) || alltas.getCreatedDate().equals(dateTo))) {
                 TasksBetween.add(alltas);
             }
         }
-        return TasksBetween;
+        return calculateTotalTime(TasksBetween);
     }
 
     public List<Task> getTasksOfLoggedUser(User loggedUser) {
         List<Task> allspecTasks = new ArrayList();
         for (Task alltas : alltasks) {
-            if(alltas.getUserLastName().equals(loggedUser.getLName())){
+            if (alltas.getUserLastName().equals(loggedUser.getLName())) {
                 allspecTasks.add(alltas);
             }
         }
         return allspecTasks;
     }
 
+    private List<Task> calculateTotalTime(List<Task> lisToFilter) {
+        HashMap<String, String> datesStored = new HashMap<String, String>(); //key is date
+        
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MMM-dd");
+        
+        List<Task> filteredList = new ArrayList();
+        
+        for (Task task : lisToFilter) {
+            Task tsk = new Task(task.getTask(), task.getNote(), task.getCreatedDate(), task.getStartTime(), task.getEndTime(), task.getSpentTime());
+            if (datesStored.get(tsk.getCreatedDate().toString()) != null) {
+        
+                for (Task taskAlreadyInList : filteredList) {
+                            LocalDate ld = new java.sql.Date( taskAlreadyInList.getCreatedDate().getTime() ).toLocalDate();
+                            LocalDate ld2 = new java.sql.Date( tsk.getCreatedDate().getTime() ).toLocalDate();
+                    if (formatter.format(ld).equals(formatter.format(ld2))) {
+                        taskAlreadyInList.setSpentTime(taskAlreadyInList.getSpentTime() + tsk.getSpentTime());
+                    }
+                }
+            } else {
+                datesStored.put(tsk.getCreatedDate().toString(), "None");
+                filteredList.add(tsk);
+            }
+        }
+        return filteredList;
+    }
 
-    
-
-    
 }
