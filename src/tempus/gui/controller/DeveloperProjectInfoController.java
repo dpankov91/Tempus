5062985980b33b66037c7cc527dfc2466802a3d2
dpankov91 <dpankov91@gmail.com>
2020-05-30 23:10:15 +0300

    project info window

--- src/tempus/gui/controller/DeveloperProjectInfoController.java
+++ src/tempus/gui/controller/DeveloperProjectInfoController.java
@@ -8,10 +8,20 @@ package tempus.gui.controller;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.util.converter.IntegerStringConverter;
+import tempus.be.Project;
+import tempus.gui.model.ProjectModel;
 
 /**
  * FXML Controller class
@@ -21,24 +31,36 @@ import javafx.scene.control.Label;
 public class DeveloperProjectInfoController implements Initializable {
 
     @FXML
-    private JFXPasswordField txt_password;
+    private TableView<Project> tblProjInfo;
     @FXML
-    private JFXTextField txt_author;
+    private TableColumn<?, ?> colName;
     @FXML
-    private JFXTextField txt_company;
-    @FXML
-    private JFXTextField txt_email;
-    @FXML
-    private JFXTextField txt_address;
-    @FXML
-    private JFXTextField txt_date;
+    private TableColumn<?, ?> colDescription;
 
+    private ProjectModel projModel;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        projModel = ProjectModel.getInstance();
+        setUpTableView();
     }    
     
+    private void setUpTableView() {
+        tblProjInfo.setEditable(true);
+
+        colName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colDescription.setCellValueFactory(new PropertyValueFactory<>("description"));
+  
+        loadTableView();
+    }
+    
+    private void loadTableView() {
+        tblProjInfo.getItems().clear();
+        List<Project> allProjects = projModel.getAllProjects();
+        ObservableList<Project> projects = FXCollections.observableArrayList();
+        projects.addAll(allProjects);
+        tblProjInfo.setItems(projects);
+    }
 }
